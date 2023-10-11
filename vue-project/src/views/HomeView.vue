4299456c2c58c2983decc9cd14034f85151f7495
ernestromen <ernestromen1996@gmail.com>
2023-10-11 21:19:04 +0300

    first commit

---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+  <h1>welcome bitch</h1>
+    <!-- <TheWelcome /> -->
+  </main>
+</template>
