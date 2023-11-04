038affdd5117a97e8462da8b999bf9f01b1c80bd
unknown <ernestromen1996@gmail.com>
2023-11-04 07:29:29 -0700

    added docker compose

---
+++ backend/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/show-all-favorites', 'App\Http\Controllers\PageController@showAllFavorites')->name('favorite_list');
+Route::get('/get-all-favorites', 'App\Http\Controllers\PageController@getAllFavorites');
+Route::delete('/delete-favorite-by-id/{id}', 'App\Http\Controllers\PageController@deleteById')->name('delete_favorite_by_id');
+Route::post('/add-favorite', 'App\Http\Controllers\PageController@store');
+Route::delete('/delete-favorite/{favoriteCity}', 'App\Http\Controllers\PageController@delete')->name('delete_favorite');
---
+++ backend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: '0.0.0.0',
+            port: 5173,
+          },
+          
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
